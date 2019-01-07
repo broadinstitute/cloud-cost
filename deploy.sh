ad732ee46c1c78424854aa47cfde6c31383b7a0d
Qi Wang <qi@broadinstitute.org>
2019-01-07 15:45:20 -0500

    remove old gcloud

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,6 @@ java -jar ~/.ivy2/cache/com.github.os72/protoc-jar/jars/protoc-jar-3.6.0.jar -I=
 
 #protoc -I=./protobuf/src/main/protobuf/ --descriptor_set_out=./protobuf/target/ccm.pb ./protobuf/src/main/protobuf/ccm.proto
 # deploy cloud enpoint
-gcloud endpoints services deploy ./protobuf/target/ccm.pb ./kubernetes/api_config.yaml
+google-cloud-sdk/bin/gcloud endpoints services deploy ./protobuf/target/ccm.pb ./kubernetes/api_config.yaml
 # creating kubernetes cluster
-kubectl create -f ./kubernetes/kub_service_deploy.yaml
+google-cloud-sdk/bin/kubectl create -f ./kubernetes/kub_service_deploy.yaml
