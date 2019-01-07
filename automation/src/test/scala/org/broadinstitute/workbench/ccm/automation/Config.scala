d4be7bb03a1f71a790617da421ef75cea52863ec
Qi Wang <qi@broadinstitute.org>
2019-01-07 16:20:46 -0500

    fix tests

--- automation/src/test/scala/org/broadinstitute/workbench/ccm/automation/Config.scala
+++ automation/src/test/scala/org/broadinstitute/workbench/ccm/automation/Config.scala
@@ -9,7 +9,7 @@ import ciris._
 object AutomationTestConfig {
   val config: IO[AutomationTestConfig] = loadConfig(
     propF[IO, Option[String]]("host"),
-    propF[IO, Option[Int]]("grpc.port")
+    propF[IO, Option[Int]]("grpc.automation.port")
   ) { (hostOpt, grpcPortOpt) =>
     val host = hostOpt.getOrElse("127.0.0.1")
     val grpcPort = grpcPortOpt.getOrElse(9999)
