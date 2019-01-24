265b4b078a03f1f2c94719bed5f9a2c81b459825
ansingh7115 <singh.an@husky.neu.edu>
2019-01-24 02:53:27 -0500

    refactoring

--- core/src/test/scala/org/broadinstitute/workbench/ccm/Generators.scala
+++ core/src/test/scala/org/broadinstitute/workbench/ccm/Generators.scala
@@ -3,6 +3,5 @@ package org.broadinstitute.workbench.ccm
 import org.scalacheck.{Arbitrary, Gen}
 
 object Generators {
-  val genCpu = Gen.alphaStr.map(Cpu)
   val genBootDiskSizedGb = Gen.posNum[Int].map(BootDiskSizeGb)
 }
